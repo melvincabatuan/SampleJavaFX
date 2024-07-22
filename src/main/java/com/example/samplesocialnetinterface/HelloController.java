5d0a773651cd9b024e685c2c68a7b2345e5051d8
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2024-07-22 12:55:27 +0800

    Update HelloController.java
    -added Initializable

--- src/main/java/com/example/samplesocialnetinterface/HelloController.java
+++ src/main/java/com/example/samplesocialnetinterface/HelloController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HelloController implements Initializable {
     
     @FXML
     private ListView<String> listView;
